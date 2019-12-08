fd8bcb839ec3fc34f4eb9293c925cc4dc45cd2c1
Matt <goodger91@gmail.com>
2019-12-08 00:31:56 +0000

    Prototype completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,13 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AboutComponent } from "./about/about.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./navigation/header/header.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
-import { HeaderComponent } from './navigation/header/header.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from "./notfound/notfound.component";
 
 @NgModule({
   declarations: [
